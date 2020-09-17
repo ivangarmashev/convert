b9a0ea973380a304086a06bd669f79ba7170379b
ivangarmashev <ivan.garmashev@gmail.com>
2020-09-17 11:42:26 +0300

    Init

---
+++ covert.py
@@ -0,0 +1,17 @@
+def convert_base(num, to_base=10, from_base=10):
+    if isinstance(num, str):
+        n = int(num, from_base)
+    else:
+        n = int(num)
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if n < to_base:
+        return alphabet[n]
+    else:
+        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
+
+
+a = (input())
+b = convert_base(a, 10, 36)
+print(b)
+print(b)
+print(b)
